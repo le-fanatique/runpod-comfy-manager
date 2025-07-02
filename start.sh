da3e00a26a8706299977a5e8229bd4163389a1df
3dWar <42842632+le-fanatique@users.noreply.github.com>
2025-07-02 14:13:48 +0200

    Update start.sh
    kill before start

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+systemctl stop nginx 2>/dev/null || true
+systemctl disable nginx 2>/dev/null || true
+pkill -f nginx || true
+
 echo "https://$RUNPOD_POD_ID-8889.proxy.runpod.net/"
 cd /workspace/runpod-comfy-manager
 source venv/bin/activate
